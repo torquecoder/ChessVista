f8a3b0d356ab3a69d75a6da1442f73fa1c449ad6
torquecoder <torquecode@gmail.com>
2017-06-02 19:31:48 +0530

    Image Generation possible

--- training_image_grabber.py
+++ training_image_grabber.py
@@ -1,7 +1,32 @@
 from selenium import webdriver
+import numpy as np
+import os
 
-driver = webdriver.PhantomJS() # or add to your PATH
-driver.set_window_size(1024, 768) # optional
-driver.get('https://google.com/')
-driver.save_screenshot('screen.png') # save a screenshot to disk
+def getRandomFEN():
+    fen_chars = list('1KQRBNPkqrbnp')
+    pieces = np.random.choice(fen_chars, 64)
+    fen = '/'.join([''.join(pieces[i * 8 : (i + 1) * 8]) for i in range(8)])
+    return fen
+
+folder_name = "generated_images"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+pieceSets = ["cburnett", "merida", "alpha", "pirouetti", "chessnut", "chess7", "reillycraig", "companion", "fantasy", "spatial", "shapes", "letter"]
+
+# Total chessboards to be grabbed
+N = 20
+image_no = 1
+
+while image_no <= N:
+    FEN = getRandomFEN()
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1024, 768)
+    driver.get('https://lichess.org/analysis/standard/' + FEN)
+    FEN = FEN.replace('/','-')
+    file_name = FEN + '.png'
+    driver.save_screenshot('generated_images/' + file_name)
+    print ("Saved Image ", image_no)
+    image_no += 1
+
 driver.quit()
