3d2a570f5fd9bcb4333f98bf113b7983cc92d03e
torquecoder <torquecode@gmail.com>
2017-06-14 22:41:15 +0530

    user tiles generated. Awesome! :)

--- app.py
+++ app.py
@@ -9,11 +9,16 @@ import uuid
 
 if not os.path.exists('uploads'):
     os.makedirs('uploads')
+if not os.path.exists('user_tiles'):
+    os.makedirs('user_tiles')
+    
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+TILES_OUTPUT_FOLDER = 'user_tiles'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TILES_OUTPUT_FOLDER'] = TILES_OUTPUT_FOLDER
 
 
 def allowed_file(filename):
@@ -43,7 +48,8 @@ def upload_file():
             file.filename = str(uuid.uuid4()) + '.' + file.filename.rsplit('.', 1)[1].lower() # Some unique file name
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            chess_board_recognizer.generateTileset(file.filename, app.config['UPLOAD_FOLDER'], app.config['TILES_OUTPUT_FOLDER'])
+            #return redirect(url_for('uploaded_file', filename=filename))
 
     return '''
     <!doctype html>
