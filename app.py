fd9d1a3a21a160360390c58b6bdab13e17f618b7
torquecoder <torquecode@gmail.com>
2017-06-14 23:29:33 +0530

    tester code updated and changes incorporated in app.py

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from werkzeug.utils import secure_filename
 from flask import send_from_directory
 import chess_board_recognizer
 import uuid
+import tester
 
 
-if not os.path.exists('uploads'):
-    os.makedirs('uploads')
+if not os.path.exists('user_chessboards'):
+    os.makedirs('user_chessboards')
 if not os.path.exists('user_tiles'):
     os.makedirs('user_tiles')
-    
-UPLOAD_FOLDER = 'uploads'
+
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+UPLOAD_FOLDER = 'user_chessboards'
 TILES_OUTPUT_FOLDER = 'user_tiles'
 
 app = Flask(__name__)
@@ -33,22 +35,31 @@ def uploaded_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+
     if request.method == 'POST':
+
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No image part')
             return redirect(request.url)
+
         file = request.files['file']
+
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No image selected')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
+
             file.filename = str(uuid.uuid4()) + '.' + file.filename.rsplit('.', 1)[1].lower() # Some unique file name
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             chess_board_recognizer.generateTileset(file.filename, app.config['UPLOAD_FOLDER'], app.config['TILES_OUTPUT_FOLDER'])
+            tiles_array = []
+            tiles_array.append("train_data/white_queen/white_queen5.png")
+            tester.testTiles(tiles_array)
             #return redirect(url_for('uploaded_file', filename=filename))
 
     return '''
--- tester.py
+++ tester.py
@@ -8,38 +8,45 @@ import numpy as np
 import cv2
 
 
-classes = ['black_bishop', 'black_king', 'black_knight', 'black_pawn', 'black_queen', 'black_rook', 'blank', \
-'white_bishop', 'white_king', 'white_knight', 'white_pawn', 'white_queen', 'white_rook']
+def testTiles(tile_paths):
 
-session = tf.Session()
-saver = tf.train.import_meta_graph('trained_model/trained_model-15000.meta')
-saver.restore(session, tf.train.latest_checkpoint('trained_model/'))
-graph = tf.get_default_graph()
 
-y_pred = graph.get_tensor_by_name("y_pred:0")
+    classes = ['black_bishop', 'black_king', 'black_knight', 'black_pawn', 'black_queen', 'black_rook', 'blank', \
+    'white_bishop', 'white_king', 'white_knight', 'white_pawn', 'white_queen', 'white_rook']
 
-# First, load the image
-dir_path = os.path.dirname(os.path.realpath(__file__))
-image_path="/test_data/blank/blank12.png"
-filename = dir_path + image_path
-image_size = 32
-images = []
-image = cv2.imread(filename)
+    session = tf.Session()
+    saver = tf.train.import_meta_graph('trained_model/trained_model-15000.meta')
+    saver.restore(session, tf.train.latest_checkpoint('trained_model/'))
+    graph = tf.get_default_graph()
 
-images.append(image)
-images = np.array(images, dtype = np.uint8)
-images = images.astype('float32')
-images = images / 255
 
-x= graph.get_tensor_by_name("x:0")
-img_size = 32
-num_channels = 3
-img_size_flat = img_size * img_size * num_channels
-x_batch = images.reshape(1, img_size_flat)
-y_true = graph.get_tensor_by_name("y_true:0")
-y_test_images = np.zeros((1, 13)) # 13 => number of classes
-feed_dict_testing = {x: x_batch, y_true: y_test_images}
-result = session.run(y_pred, feed_dict = feed_dict_testing)
-print(result)
-index = np.argmax(result)
-print(classes[index])
+
+    y_pred = graph.get_tensor_by_name("y_pred:0")
+
+    for image_path in tile_paths:
+
+        # First, load the image
+        #dir_path = os.path.dirname(os.path.realpath(__file__))
+        #image_path="/test_data/blank/blank12.png"
+        filename = image_path
+        image_size = 32
+        images = []
+        image = cv2.imread(filename)
+
+        images.append(image)
+        images = np.array(images, dtype = np.uint8)
+        images = images.astype('float32')
+        images = images / 255
+
+        x= graph.get_tensor_by_name("x:0")
+        img_size = 32
+        num_channels = 3
+        img_size_flat = img_size * img_size * num_channels
+        x_batch = images.reshape(1, img_size_flat)
+        y_true = graph.get_tensor_by_name("y_true:0")
+        y_test_images = np.zeros((1, 13)) # 13 => number of classes
+        feed_dict_testing = {x: x_batch, y_true: y_test_images}
+        result = session.run(y_pred, feed_dict = feed_dict_testing)
+        #print(result)
+        index = np.argmax(result)
+        print(classes[index])
