d748c494004441310b85cee3726fe9d55d73424f
torquecoder <torquecode@gmail.com>
2017-06-14 11:42:00 +0530

    image uploading flask app added

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No image part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No image selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return '''
+    <!doctype html>
+    <title>Upload Chessboard Image</title>
+    <h1>Upload Chessboard Image</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
